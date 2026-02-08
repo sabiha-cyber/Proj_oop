0ef9b1fb943b313811cd16159d681f91c406eee4
lipglossuserr <samia04t@gmail.com>
2026-02-09 00:50:57 +0600

    Fixed bugs in User class + added password validation

--- User.h
+++ User.h
@@ -9,26 +9,26 @@ private:
     int userId;
     std::string username;
     std::string password;
-    std::vector<int> friendIds;           // still here – most practical for now
+    std::vector<int> friendIds;           
 
 public:
     User();
     User(int id, const std::string& uname, const std::string& pass);
 
-    // Getters
+  
     int                 getUserId()     const;
     std::string         getUsername()   const;
     std::string         getPassword()   const;
 
-    // Friend list access (read-only)
+    
     const std::vector<int>& getFriendIds() const;
 
-    // Friend management – called only by FriendService
+   
     void addFriend(int friendUserId);
     bool hasFriend(int otherId) const;
 
-    // Display
+   
     void printBasicInfo() const;
 };
 
-#endif // USER_H
+#endif 
