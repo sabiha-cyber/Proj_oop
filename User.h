95e03e9b574410ed01db52e2f4901d2084340460
Samia Tasmim <samia04t@gmail.com>
2026-03-08 02:28:51 +0600

    Enhance User class with post management features
    Added myPosts vector and methods for managing posts.

--- User.h
+++ User.h
@@ -4,31 +4,35 @@
 #include <string>
 #include <vector>
 
+class Post;  
+
 class User {
 private:
     int userId;
     std::string username;
     std::string password;
-    std::vector<int> friendIds;           
+    std::vector<int> friendIds;
+    std::vector<Post*> myPosts;          
 
 public:
     User();
     User(int id, const std::string& uname, const std::string& pass);
 
-  
     int                 getUserId()     const;
     std::string         getUsername()   const;
     std::string         getPassword()   const;
 
-    
     const std::vector<int>& getFriendIds() const;
 
-   
     void addFriend(int friendUserId);
     bool hasFriend(int otherId) const;
+    void removeFriend(int friendUserId);  // ← NEW: remove/unfriend
 
-   
     void printBasicInfo() const;
+
+    void                addPost(Post* post);
+    const std::vector<Post*>& getPosts() const;
+    void                showMyPosts() const;
 };
 
-#endif 
+#endif
